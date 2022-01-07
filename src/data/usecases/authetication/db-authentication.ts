5a10c1cbc7b705ab1fde0e2696e1c1dc08630438
Robério Guedelho <roberiocrow@gmail.com>
2022-01-07 13:18:29 -0300

    refactor: rename update whith apdateAccessToken

--- src/data/usecases/authetication/db-authentication.ts
+++ src/data/usecases/authetication/db-authentication.ts
@@ -18,7 +18,7 @@ export class DbAuthentication implements Authentication {
       return null
     }
     const accessToken = await this.tokenGenerator.generate(account.id)
-    await this.updateAccessTokenRepository.update(account.id, accessToken)
+    await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
     return accessToken
   }
 }
