1abe208c4c63458ce5883ddc3bba8e559e4bb4ba
Robério Guedelho <roberiocrow@gmail.com>
2022-01-10 13:33:33 -0300

    refactor: change routes to use filesystem instead of fast-glob

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeLoginController } from '../factories/login/login-factory'
